63e7eb998719eadcfd686280b7f6696a9b0753b3
graziplay344 <164170811+graziplay344@users.noreply.github.com>
2024-05-29 07:48:31 -0300

    projeto de java scrip

--- script.js
+++ script.js
@@ -10,25 +10,22 @@ meuFormulario.addEventListener("submit", function(event) {
 
 //Função somar
 function somar() {
-
+  
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let primeiro_desconto = Number(document.getElementById("primeiro_numero_inteiro").value);
-
+  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
+  
   //Captura o que está dentro do segundo input no formulário
   //Number - Transforma o texto capturado em Numero
-  let segundo_desconto= Number(document.getElementById("segundo_numero_inteiro").value);
-
-  ptura o que está dentro do segundo input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let terceiro_desconto= Number(document.getElementById("terceiro_desconto").value);
+  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
+  
   //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
   //variável "segundo_numero_inteiro"
   //E a variável "resultado_soma" recebe o valor da soma
-  let resultado_soma=primeiro_desconto+ segundo_desconto+terceiro_desconto;
+  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
 
   //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
   document.getElementById("resultado_soma").value = resultado_soma;
-
-
+  
+  
 }
